0b3c374e5b9f5fe374a2f6993cf7c86c6ac9d6d9
J <j@192.168.1.4>
2025-02-08 19:42:45 -0300

    Remove duplicate people page

--- app/(authenticated)/people/page.tsx
+++ app/(authenticated)/people/page.tsx
@@ -1,14 +1,6 @@
-import { TopNav } from "@/components/top-nav"
 import { PeopleView } from "@/components/people-view"
-import { TestComponent } from "@/components/test-component"
 
 export default function PeoplePage() {
-  return (
-    <>
-      <TopNav />
-      <TestComponent />
-      <PeopleView />
-    </>
-  )
+  return <PeopleView />
 }
 
--- app/people/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PeopleView } from "@/components/people-view"
-
-export default function PeoplePage() {
-  return <PeopleView />
-}
-
